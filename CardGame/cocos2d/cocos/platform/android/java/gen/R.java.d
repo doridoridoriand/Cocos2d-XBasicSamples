6de9be85b0d65f5bf9f2f6af57b6f48dbb9c08f4
doridoridoriand <i.am.eager.for.peace@gmail.com>
2015-09-05 18:33:57 +0900

    add CardGame project

---
+++ CardGame/cocos2d/cocos/platform/android/java/gen/R.java.d
@@ -0,0 +1,1 @@
+ : /Users/dorian/works/Cocos2d-xBasicSamples/CardGame/cocos2d/cocos/platform/android/java/bin/AndroidManifest.xml \
---
+++ CardGame/proj.android/gen/R.java.d
@@ -0,0 +1,6 @@
+/Users/dorian/works/Cocos2d-xBasicSamples/CardGame/proj.android/gen/com/doridorian/projects/R.java \
+ : /Users/dorian/works/Cocos2d-xBasicSamples/CardGame/proj.android/res/drawable-hdpi/icon.png \
+/Users/dorian/works/Cocos2d-xBasicSamples/CardGame/proj.android/res/drawable-ldpi/icon.png \
+/Users/dorian/works/Cocos2d-xBasicSamples/CardGame/proj.android/res/drawable-mdpi/icon.png \
+/Users/dorian/works/Cocos2d-xBasicSamples/CardGame/proj.android/res/values/strings.xml \
+/Users/dorian/works/Cocos2d-xBasicSamples/CardGame/proj.android/bin/AndroidManifest.xml \
